679663f22cdbd732018f9745afe510159e7a676c
JacobYim <dlagudah@gmail.com>
2022-04-08 10:09:29 -0400

    add sh

--- py/main.py
+++ py/main.py
@@ -19,76 +19,76 @@ def process(particle_number, window_width, window_height, T, k, g, dt, radius_ra
 
     filename = "HIST_{}_{}_{}_{}_{}_{}_{}_{}_{}".format(particle_number, window_width, window_height, T, radius_rate, mass_rate , k, g, dt)
     length = len(list(filter(lambda x : filename in x,os.listdir(data_dir))))
-    with open(data_dir+'/'+filename+"_({})".format(length)+".csv", "a+") as f:
-        f.write("time;final_velocity;density\n")
-        density = particle_number/(window_width*window_height)
-        cnt_hit_cur_v = 0
-        vys = []
-        t = 0.0
-        mp_m = 100
-        mp_r = 50
-        fp_m = mp_m*mass_rate
-        fp_r = mp_r*radius_rate
+    f = open(data_dir+'/'+filename+"_({})".format(length)+".csv", "a+")
+    f.write("time;final_velocity;density\n")
+    density = particle_number/(window_width*window_height)
+    cnt_hit_cur_v = 0
+    vys = []
+    t = 0.0
+    mp_m = 100
+    mp_r = 50
+    fp_m = mp_m*mass_rate
+    fp_r = mp_r*radius_rate
 
-        num_top = 0
-        num_bottom = 0
-        num_right = 0
-        num_left = 0
+    num_top = 0
+    num_bottom = 0
+    num_right = 0
+    num_left = 0
 
-        mp = MainParticle(window_width, window_height, g, dt, mp_m, mp_r)
-        fps = [FreeParticle(window_width, window_height, g, dt, fp_m, fp_r, T, 1, mp, None, mp.vx, mp.vy) for _ in range(particle_number)]
-        while (True) :
-            for i in range(0, len(fps)) :
-                # update depend on whether collision occurred or not
-                [collision_check, mp.vx, mp.vy, fps[i].x,fps[i].y, fps[i].vx, fps[i].vy] = calculate_particles_collision(fps[i], mp)
-                # calculate next time steps of free particles
-                fps[i].update(mp)
-                # update by the 
-                if (check_remove_paricle(fps[i], mp, window_height, window_width)) :
-                        fps[i] = None
-            fps = list(filter(lambda x: x is not None, fps))   
-            mp.update()
-            # add new particles
-            [fps, num_bottom, num_top, num_right, num_left] = calculate_paticles_to_add(mp, fps, fp_m, fp_r, density, dt, k, g, T, window_width, window_height, num_bottom, num_top, num_right, num_left)           
-            vwx = mp.vx
-            vwy = mp.vy
+    mp = MainParticle(window_width, window_height, g, dt, mp_m, mp_r)
+    fps = [FreeParticle(window_width, window_height, g, dt, fp_m, fp_r, T, 1, mp, None, mp.vx, mp.vy) for _ in range(particle_number)]
+    while (True) :
+        for i in range(0, len(fps)) :
+            # update depend on whether collision occurred or not
+            [collision_check, mp.vx, mp.vy, fps[i].x,fps[i].y, fps[i].vx, fps[i].vy] = calculate_particles_collision(fps[i], mp)
+            # calculate next time steps of free particles
+            fps[i].update(mp)
+            # update by the 
+            if (check_remove_paricle(fps[i], mp, window_height, window_width)) :
+                    fps[i] = None
+        fps = list(filter(lambda x: x is not None, fps))   
+        mp.update()
+        # add new particles
+        [fps, num_bottom, num_top, num_right, num_left] = calculate_paticles_to_add(mp, fps, fp_m, fp_r, density, dt, k, g, T, window_width, window_height, num_bottom, num_top, num_right, num_left)           
+        vwx = mp.vx
+        vwy = mp.vy
 
-            if (num_bottom > 0 ):
-                for i in range(0, int(num_bottom)):
-                    fps.append(FreeParticle(window_width, window_height, g, dt, fp_m, fp_r, T, k, mp, "bottom", vwx, vwy))
-                    num_bottom = num_bottom - 1
-                
+        if (num_bottom > 0 ):
+            for i in range(0, int(num_bottom)):
+                fps.append(FreeParticle(window_width, window_height, g, dt, fp_m, fp_r, T, k, mp, "bottom", vwx, vwy))
+                num_bottom = num_bottom - 1
             
-            if (num_top > 0 ):
-                for i in range(0, int(num_top)):
-                    fps.append(FreeParticle(window_width, window_height, g, dt, fp_m, fp_r, T, k, mp, "top", vwx, vwy))
-                    num_top = num_top - 1
-                
+        
+        if (num_top > 0 ):
+            for i in range(0, int(num_top)):
+                fps.append(FreeParticle(window_width, window_height, g, dt, fp_m, fp_r, T, k, mp, "top", vwx, vwy))
+                num_top = num_top - 1
             
-            if (num_right > 0 ):
-                for i in range(0, int(num_right)):
-                    fps.append(FreeParticle(window_width, window_height, g, dt, fp_m, fp_r, T, k, mp, "right", vwx, vwy))
-                    num_right = num_right - 1
-                
+        
+        if (num_right > 0 ):
+            for i in range(0, int(num_right)):
+                fps.append(FreeParticle(window_width, window_height, g, dt, fp_m, fp_r, T, k, mp, "right", vwx, vwy))
+                num_right = num_right - 1
             
-            if (num_left > 0 ):
-                for i in range(0, int(num_left)):
-                    fps.append(FreeParticle(window_width, window_height, g, dt, fp_m, fp_r, T, k, mp, "left", vwx, vwy))
-                    num_left = num_left - 1
-            
-            save(f, t, fps, mp, window_width, window_height)
-            # print(t, round(mp.vy, 5), round(mp.vx, 5), density, len(fps)/(window_width*window_height))
-            t = t+dt
+        
+        if (num_left > 0 ):
+            for i in range(0, int(num_left)):
+                fps.append(FreeParticle(window_width, window_height, g, dt, fp_m, fp_r, T, k, mp, "left", vwx, vwy))
+                num_left = num_left - 1
+        
+        save(f, t, fps, mp, window_width, window_height)
+        # print(t, round(mp.vy, 5), round(mp.vx, 5), density, len(fps)/(window_width*window_height))
+        t = t+dt
 
-            
-            vys.append(mp.vy)
-            if len(vys) > 500 :
-                if abs(sum(vys[-500:])/500 - mp.vy) < 0.1 :
-                    cnt_hit_cur_v += 1
-                    if cnt_hit_cur_v > 50 :
-                        break
+        
+        vys.append(mp.vy)
+        if len(vys) > 5000 :
+            if abs(sum(vys[-5000:])/5000 - mp.vy) < 0.1 :
+                cnt_hit_cur_v += 1
+            if cnt_hit_cur_v > 50 :
+                break
 
-    # f.close()
+    f.close()
 
 if __name__ == '__main__':
 
@@ -104,22 +104,23 @@ if __name__ == '__main__':
     data_dir = 'result_'+sim['sim_name']
     if not data_dir in os.listdir() :
         os.mkdir(data_dir)
-    for num in range(procs_num):
-        if sim['sim_name'] == "T" :
-            for T in sim['range'] :
-                p =Process(target=process, args=(particle_number,                       window_width, window_height, T, k, g, dt, radius_rate, mass_rate, data_dir))
-                p.start()
-        elif sim['sim_name'] == "RR" :
-            for rr in sim['range'] :
-                p =Process(target=process, args=(particle_number,                       window_width, window_height, T, k, g, dt, rr, mass_rate, data_dir))
-                p.start()
-        elif sim['sim_name'] == "MR" :
-            for mr in sim['range'] :
-                p =Process(target=process, args=(particle_number,                       window_width, window_height, T, k, g, dt, radius_rate, mr, data_dir))
-                p.start()
-        elif sim['sim_name'] == "D" :
-            for density in sim['range'] :
-                p =Process(target=process, args=(int(density*window_height*window_width),    window_width, window_height, T, k, g, dt, radius_rate, mass_rate, data_dir))
-                p.start()
+    for group in range(4) :
+        for num in range(int(procs_num/4)):
+            if sim['sim_name'] == "T" :
+                for T in sim['range'] :
+                    p =Process(target=process, args=(particle_number,                       window_width, window_height, T, k, g, dt, radius_rate, mass_rate, data_dir))
+                    p.start()
+            elif sim['sim_name'] == "RR" :
+                for rr in sim['range'] :
+                    p =Process(target=process, args=(particle_number,                       window_width, window_height, T, k, g, dt, rr, mass_rate, data_dir))
+                    p.start()
+            elif sim['sim_name'] == "MR" :
+                for mr in sim['range'] :
+                    p =Process(target=process, args=(particle_number,                       window_width, window_height, T, k, g, dt, radius_rate, mr, data_dir))
+                    p.start()
+            elif sim['sim_name'] == "D" :
+                for density in sim['range'] :
+                    p =Process(target=process, args=(int(density*window_height*window_width),    window_width, window_height, T, k, g, dt, radius_rate, mass_rate, data_dir))
+                    p.start()
     # p =Process(target=process, args=(particle_number, window_width, window_height, T, k, g, dt))
     # p.start()
--- py/sim_T.sh
+++ py/sim_T.sh
@@ -3,5 +3,9 @@
 #SBATCH -J final_vel_w_teperature   # job name
 #SBATCH -o final_vel_w_teperature.%j.out   # standard output and error log
 #SBATCH -t 70:00:00               # Run time (hh:mm:ss)
+#SBATCH --nodes=4              
+#SBATCH --ntasks-per-node=32      
+#SBATCH --mail-user=jyim@buffalo.edu
+#SBATCH --mem=250000
 
 python3 main.py T
