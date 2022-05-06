e35d9d4497beedd7970cbfad69664b8024fa7ada
JacobYim <dlagudah@gmail.com>
2022-05-06 01:49:54 -0400

    segmentate MR part

--- py/main.py
+++ py/main.py
@@ -99,7 +99,7 @@ if __name__ == '__main__':
     elif sys.argv[1] == "RR" :
         sim = {"sim_name":"RR", "range": list(map(lambda x : x,  range( 1, 100)))} # fr/mr
     elif sys.argv[1] == "MR" :
-        sim = {"sim_name":"MR", "range": list(map(lambda x : x,  range( 1, 100)))} # fm/mm
+        sim = {"sim_name":"MR", "range": list(map(lambda x : x, range( 1 if int(sys.argv[2]) == 1 else (int(sys.argv[2])-1)*10, int(sys.argv[2])*10)))} # fm/mm
     elif sys.argv[1] == "D" :
         sim = {"sim_name":"D",  "range": list(map(lambda x : x/1000, range( 1 if int(sys.argv[2]) == 1 else (int(sys.argv[2])-1)*10, int(sys.argv[2])*10)))}
     else :
---
+++ py/sim_MR8.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=MR8_multiprocessing
+#SBATCH --output=MR8_multiprocessing.out
+#SBATCH --time 70:00:00               # Run time (hh:mm:ss)
+#SBATCH --mail-user=jyim@buffalo.edu
+#SBATCH --mail-type=ALL
+
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --exclusive
+#SBATCH --partition=general-compute
+
+#SBATCH --mem=48000
+#SBATCH --ntasks-per-node=12            
+
+module load python/anaconda
+ulimit -s unlimited
+
+python main.py MR 8
